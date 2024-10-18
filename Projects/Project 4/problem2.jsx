b9869290b440d8280b9ebe92b6ef47aa2208e800
unclethien <dxn210021@utdallas.edu>
2024-10-18 09:56:59 -0500

    update

--- Projects/Project 4/problem2.jsx
+++ Projects/Project 4/problem2.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
 import States from "./components/States/index.jsx";
 
-ReactDOM.render(<States />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <States />
+  </React.StrictMode>
+);
